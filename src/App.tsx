0e5a9dbdc00db34f097e0215a63b0846aca8f052
panwilku <marcin.wilk.503@gmail.com>
2025-05-04 09:24:08 +0900

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import ItemCard from './Card'
+import NavBar from './NavBar'
 
 function App() {
 
   return (
     <>
       <div className='flex flex-col max-w-[1920px] w-full bg-amber-200 items-center h-screen'>
-        siema
-        <ItemCard ></ItemCard>
+        <NavBar></NavBar>
       </div>
     </>
   )
